926f3605c72f7b80f5c2a13431f2d9730a950af2
Al <awessonn@gmail.com>
2023-04-26 13:30:23 -0700

    created .env file for connection.

--- config/connections.js
+++ config/connections.js
@@ -1,10 +1,11 @@
+require('dotenv').config();
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
     host: 'localhost',
-    user: 'root',
-    password: 'AlsSQL1990!',
-    database: '/'
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: ''
 }).promise();
 
 module.exports = connection;
